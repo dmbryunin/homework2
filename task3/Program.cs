9bb7d5b98570e6ad6b048c3a1757e7b02dc6f00d
dmitry <sdmitry>
2022-12-23 23:35:00 +0500

    third task

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+nat1:
+
+Console.WriteLine("vvedite chislo ot 1 do 7");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (a<1 || a>7)
+    goto nat1;
+if (a==6 || a==7)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
