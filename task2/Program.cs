c9ec061e5d9c2bad218da20b72b7c3d1bd795a09
dmitry <sdmitry>
2022-12-24 12:18:26 +0500

    second task

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("vvedite chislo");
+
+int n=Convert.ToInt32(Console.ReadLine());
+
+if (Math.Abs(n)<100)
+    Console.WriteLine("net tretey cifri");
+else{
+    while (n>1000)
+        n=n/10;
+    Console.WriteLine(n%10);
+}
